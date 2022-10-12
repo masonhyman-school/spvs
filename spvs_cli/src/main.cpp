39d89d468bea1c34aa75389f2ad4e39b1e0e81a2
Mason Hyman <mason@Masons-MacBook-Pro-6.local>
2022-10-12 16:29:34 -0400

    Commiting built SFML library and new Makefile

--- spvs_cli/src/main.cpp
+++ spvs_cli/src/main.cpp
@@ -3,7 +3,15 @@
 
 using namespace std;
 
+static bool port_is_open(const string& address, int port) {
+	sf::IpAddress addr(127, 0, 0, 1);
+	return (sf::TcpSocket().connect(addr, port) == sf::Socket::Status::Done);
+}
+
 int main(int argc, char ** argv) {
-	cout << "Hello SPVS" << endl;
+	cout << "Port 1947 : ";
+	if (port_is_open("localhost", 1947)) cout << "OPEN" << endl;
+	else cout << "CLOSED" << endl;
+
 	return 0;
 } 
