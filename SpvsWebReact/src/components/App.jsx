76b4f33c9ccb09fa59809ce2761f6960448d2b13
Shivam Mistry <smistry1@vols.utk.edu>
2022-10-13 17:58:48 -0400

    initial react front-end

---
+++ SpvsWebReact/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header.jsx';
+import Ip from './Ip.jsx';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Ip />
+        </div>
+    );
+}
+
+export default App;
