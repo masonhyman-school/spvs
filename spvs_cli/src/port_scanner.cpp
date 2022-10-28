8cb4f10668ad358f1ca5a93aa19a967bd5ae41c7
ankitm28 <amistry2@vols.utk.edu>
2022-10-27 20:09:39 -0400

    port-scanner_Ankit

--- spvs_cli/src/port_scanner.cpp
+++ spvs_cli/src/port_scanner.cpp
@@ -1,41 +1,92 @@
+/* port_scanner.cpp
+ * Ankit Mistry
+ * Last Updated October 27, 2022 08:08 PM
+ * Originally created by Mason Hyman, modified by me.
+ * 
+ * Scans for open ports on a given IP address or web address. Works on both stdin and file read.
+*/
+
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <errno.h>
+#include <fstream>
+#include <list>
+#include <vector>
+#include <thread>
+#include <SFML/Network.hpp>
 
 using namespace std;
-struct sockaddr_in dest;
 
-int main()
-{
-   int sockt, connct, port;
-
-   cout << "Enter the number of port: ";
-   cin >> port;
-
-   
-   if ((sockt = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-      perror("socket");
-      exit(1);
-   }
-
-   dest.sin_family = AF_INET;
-   dest.sin_port = htons(port);
-   bzero(dest.sin_zero, sizeof(dest.sin_zero));
-
-   
-   if ((connct = connect(sockt, (struct sockaddr *)&dest, sizeof(dest))) == -1) {
-      perror("connect");
-      close(sockt);
-      exit(1);
-   } else {
-      cout << port << " open" << endl;
-      exit(1);
-   }
-
-   return 0;
+void print_usage() {
+	cerr <<  "Usage: spvs [Options] {target specification}" << endl
+		  << "TARGET SPECIFICATION:" << endl
+		  << "    -f <input file name>: Input from list of hosts." << endl;
 }
+
+static bool port_is_open(const string& address, int port) {
+	return (sf::TcpSocket().connect(sf::IpAddress::resolve(address).value(), port, sf::milliseconds(100)) == sf::Socket::Status::Done);
+}
+
+void threaded_port_is_open(const string& address, int port, vector<bool>& status) {
+	cout << address << endl;	
+	if(sf::TcpSocket().connect(sf::IpAddress::resolve(address).value(), port) == sf::Socket::Status::Done) {
+		status.at(port) = 1;
+	}
+}
+
+void port_scan(const string& address, int ports) {
+	vector <bool> open_ports(ports+1, false);
+	vector <thread *> threads;
+
+	bool host_online = false;
+	
+	for (int i = 1; i <= ports; i++) {
+		if (port_is_open(address, i)) {
+			host_online = true;
+			open_ports.at(i) = true;
+			cout << "PORT " << i << " IS OPEN\n";
+		} 	
+	}
+
+	if (host_online) {
+		cout << address << " is ONLINE with open ports: " << endl;
+		for (int i = 1; i < open_ports.size(); i++) {
+			if (open_ports.at(i)) cout << "   Port " << i << " is open" << endl;
+		}
+     	cout << endl;
+	} else {
+		cout << address << " is OFFLINE." << endl;
+	}
+}
+
+int main(int argc, char ** argv) {
+		
+	list<string> hosts;
+	list<string>::iterator lit;
+   string host;
+   ifstream file;
+
+	/* PARSE USER COMMANDS */
+	if (argc == 1) { print_usage(); return 1; }	
+	else if (argc == 2) {
+		/* If only two arguments, second arg must be a hostname */
+		hosts.push_back(argv[1]);
+	}
+	else if (argc == 3) {
+		if (string(argv[1]) == "-f") {
+			/* READ HOSTS FROM FILE */
+         file.open(argv[2]);
+			if(!file.is_open()) cerr << "Could not open file " << argv[1] << endl;
+				
+			while (file >> host) hosts.push_back(host); 
+			file.close();
+			cout << "Successfully read in " << hosts.size() << " hosts" << endl;
+		}
+	} 
+
+	/* PERFORM BASIC DISCOVERY */
+	/* Essentially determining if host is online */
+	for (lit = hosts.begin(); lit != hosts.end(); lit++) {
+		port_scan(*lit, 65535);
+   }
+
+	return 0;
+} 
