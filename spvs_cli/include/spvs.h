584176a32ab64379edffcbac41fd2ca439d48f6e
Mason Hyman <mason@Masons-MacBook-Pro-8.local>
2022-10-27 22:52:23 -0400

    Implemented port range scans

--- spvs_cli/include/spvs.h
+++ spvs_cli/include/spvs.h
@@ -1,9 +1,23 @@
 #ifndef SPVS_HEADER_FILE_H
 #define SPVS_HEADER_FILE_H
 
-/* General Purpose Functions */
-void Print_Usage();
+/* Defines information for single target. */
+class Target
+{
+	public:
+		Target (std::string&);
 
+		bool ICMP_Host_Online(const std::string&);
+		void TCP_Port_Is_Open(const std::string&, const int);
+		void TCP_Scan_Ports(const std::string&, const std::vector<int>&);		
+	
+	/* Accessor / Information Functions */
+		void TCP_Print_Open_Ports();
+		const std::string& Get_Address();
+	private:
+		std::string address;
+		std::vector<int> open_ports; 
+};
 
 /* Input Parser */
 class Input
@@ -13,22 +27,27 @@ class Input
 
 		bool Arg_Exists(const std::string &option) const;
 		const std::string& Get_Next_Arg(const std::string & option) const;
+
+		std::vector<Target *> Get_Targets_From_File(const std::string&);
+		std::vector<Target *> Get_Targets_From_Stdin();
+		std::vector<int> Get_Ports_To_Scan(const std::string&);
 	private:
-		std::vector<std::string> args;
+		std::list<std::string> args;
 };
 
-/* Defines information for single target. */
-class Target
+/* Overarching SPVS Class. Drives program */
+class SPVS
 {
 	public:
-		bool ICMP_Host_Online(const std::string&);
-		void TCP_Port_Is_Open(const std::string&, const int);
-		void TCP_Scan_Ports(const std::string&, const std::vector<int>&);		
-	
-	/* Accessor / Information Functions */
-		void TCP_Print_Open_Ports();
+		SPVS (int &argc, char ** argv);
+
+		void Print_Usage();
+		void Print_Targets();
+		void Print_Ports();
 	private:
-		std::vector<int> open_ports; 
+		Input * input;
+		std::vector<Target *> targets;
+		std::vector<int> ports_to_scan;
 };
 
 #endif
--- spvs_cli/src/main.cpp
+++ spvs_cli/src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
+#include <list>
 #include <algorithm>
 #include <thread>
 #include <mutex>
+#include <chrono>
+#include <ctime>
+#include <locale>
 #include <SFML/Network.hpp>
 
 #include "spvs.h"
@@ -23,13 +28,13 @@ int main(int argc, char** argv) {
 	t.TCP_Scan_Ports(argv[1], ports);
 	
 	t.TCP_Print_Open_Ports();
-*/
 
 	Input input(argc, argv);
 	if (input.Arg_Exists("-f")) cout << "Found" << endl;
 
 	cout << input.Get_Next_Arg("-f") << endl;
-
+*/
+	SPVS(argc, argv);
 	return 0;
 }
 
@@ -38,17 +43,83 @@ int main(int argc, char** argv) {
 
 /* DEFINITIONS FOR spvs.h */
 
-/* GENERAL PURPOSE */
+/* SPVS CLASS */
+
+/*
+ * @name SPVS
+ * @brief Constructor for SPVS class. Serves as driver for program.
+ * @param[in] argc - number of args on stdin
+ * @param[in] argv - array of args from stdin
+ * @return SPVS class
+ */
+
+SPVS::SPVS(int &argc, char ** argv) {
+	vector <Target *> tmp;
+
+	if (argc == 1) Print_Usage();
+	
+	/* Begin timing information */
+	auto start = chrono::system_clock::to_time_t(chrono::system_clock::now());
+	cout << "Starting SPVS at " << ctime(&start); 
+	
+	this->input = new Input(argc, argv);
+	
+	/* Check to see if -f has been specified, if so handle the file and load targets */
+	if (this->input->Arg_Exists("-f")) {
+		this->targets = this->input->Get_Targets_From_File(this->input->Get_Next_Arg("-f"));
+	}
+
+	/* Check to see if -p has been specified, if so load port / port range */
+	if (this->input->Arg_Exists("-p")) {
+		this->ports_to_scan = this->input->Get_Ports_To_Scan(this->input->Get_Next_Arg("-p"));		
+	} else {
+		/* No ports specified, populate default */
+		/* TODO */
+	}
+	
+	/* Treat all words on stdin without '-' or '--' as targets */
+	tmp = this->input->Get_Targets_From_Stdin();
+	targets.insert(targets.end(), tmp.begin(), tmp.end());
+
+	/* Once targets have been gathered, identify modifers to default SPVS */
+		
+
+	/* End timing and print results */
+	auto end = chrono::system_clock::to_time_t(chrono::system_clock::now());
+//	Print_Targets();
+//	Print_Ports();
+}
 
 /*
  *	@name Print_Usage
  *	@brief Prints spvs usage.
  */
-void Print_Usage() {	
+void SPVS::Print_Usage() {	
 	cerr <<  "Usage: spvs [Options] {target specification}" << endl
 		 << "TARGET SPECIFICATION:" << endl
 		 << "    -f <input file name>: Input from list of hosts." << endl;
+
+	exit(1);
 }
+
+void SPVS::Print_Targets() {
+	for (int i = 0; i < targets.size(); i++) {
+		cout << targets[i]->Get_Address() << endl;
+	}
+}
+
+void SPVS::Print_Ports() {
+	for (int i = 0; i < ports_to_scan.size(); i++) {
+		cout << ports_to_scan[i] << " ";
+	}
+	cout << endl;
+}
+
+
+
+
+
+
 
 /* INPUT CLASS */
 
@@ -64,12 +135,14 @@ Input::Input(int &argc, char ** argv) {
 		this->args.push_back(string(argv[i]));
 	}
 }
+
 /*
  *	@name Arg_Exists
  *	@brief Checks if supplied argument exists in user's input
  *	@param[in] option - supplied argument to find
  *  @return true if arg was found
  */
+
 bool Input::Arg_Exists(const string& option) const {
 	return find(args.begin(), args.end(), option) != args.end();
 }
@@ -80,21 +153,149 @@ bool Input::Arg_Exists(const string& option) const {
  *	@param[in] option
  *  @return the desired argument, empty string if none exists
  */
+
 const string& Input::Get_Next_Arg(const string& option) const {
-	vector<string>::const_iterator vit;
+	list<string>::const_iterator lit;
 
 	/* Find the supplied argument and then return the following argument */
-	vit = find(args.begin(), args.end(), option);
+	lit = find(args.begin(), args.end(), option);
 	
-	if (vit != args.end() && ++vit != args.end()) {
-		return *vit;
+	if (lit != args.end() && ++lit != args.end()) {
+		return *lit;
 	} 
 
 	static const string empty = "";	
 	return empty;
 }
 
+/* 
+ *	@name Get_Targets_From_File
+ *	@brief Collects all words from file and initializes Target instance for each.
+ *	@param[in] filename - target filename
+ *  @return vector<Target *> - list of targets to be assigned to SPVS class.
+ */
+
+vector<Target *> Input::Get_Targets_From_File(const string& filename) {
+	vector<Target *> ret;
+	string s;
+	ifstream file;
+	Target * t;
+
+	file.open(filename);
+
+	if (!file.is_open()) {
+		cerr << endl << "ERROR: Could not open file: " << filename << endl;
+		exit(1);
+	}
+
+	while(file >> s) {
+		t = new Target(s);
+		ret.push_back(t);
+	}	
+
+	/* Cleanup and remove -f and filename from args */
+	file.close();	
+	
+	args.erase(find(args.begin(), args.end(), "-f"));	
+	args.erase(find(args.begin(), args.end(), filename));	
+	return ret;	
+}
+
+/* 
+ *	@name Get_Targets_From_Stdin
+ *	@brief Collects all non '-' or '--' args and creats targets for them.
+ *	@return a vector of targets to be assigned to SPVS class.
+ *
+ */
+
+vector<Target *> Input::Get_Targets_From_Stdin() {
+	list<string>::const_iterator lit;
+	vector<Target *> ret;
+	Target * t;
+
+	for (lit = args.begin(); lit !=  args.end(); lit++) {
+		if (lit->at(0) != '-') {
+			string name = *lit;
+			t = new Target(name);
+			ret.push_back(t);
+		} 
+	}
+
+	return ret;
+}
+
+vector<int> Input::Get_Ports_To_Scan(const string& input) {
+	vector<int> ret;
+
+	/* Check to see if range was specified */
+	int pos = input.find('-');
+	if (pos != string::npos) {
+		/* Split the string */
+		string l;
+		for (int i = 0; i < pos; i++) l += input[i];
+
+		string r;
+		for (int i = pos + 1; i < input.size(); i++) r += input[i];
+		
+		if (!all_of(l.begin(), l.end(), ::isdigit) || !all_of(r.begin(), r.end(), ::isdigit)) {
+			cerr << endl << "ERROR: Invalid port specified, must be a number" << endl;
+			exit(1);
+		}
+
+		int starting_port = stoi(l);
+		int ending_port = stoi(r);
+		
+		if (starting_port >= ending_port) {
+			cerr << endl << "ERROR: Starting port must not be greater than ending port" << endl;
+			exit(1);
+		} else if (ending_port > 65535 || starting_port < 1) {
+			cerr << endl << "ERROR: Port specification out of bounds. Port range must be between 1 - 65535" << endl;
+			exit(1);
+		}
+
+		for (int i = starting_port; i <= ending_port; i++) {
+			ret.push_back(i);
+		}
+	} else {
+		/* Only one port specified */
+		if (!all_of(input.begin(), input.end(), ::isdigit)) {
+			cerr << endl << "ERROR: Invalid port specified, must be a number" << endl;
+			exit(1);
+		}
+		
+		int port = stoi(input);
+	
+		if (port < 1 || port > 65535) {
+			cerr << endl << "ERROR: Port specification out of bounds. Port range must be between 1 - 65535" << endl;
+			exit(1);
+		}
+
+		ret.push_back(port);
+	}
+		
+	/* Cleanup and remove -p and port specification from args */
+	args.erase(find(args.begin(), args.end(), "-p"));	
+	args.erase(find(args.begin(), args.end(), input));	
+	return ret;
+}
+
+
+
+
+
+
+
+
 /* TARGET CLASS */
+
+/*
+ *	@name Target
+ *	@brief Constructs a Target instance, assignes its address.
+ */
+
+Target::Target(string& address) {
+	this->address = address;
+}
 
 /*
  *	@name Host_Online
@@ -137,6 +338,7 @@ void Target::TCP_Port_Is_Open(const string& address, const int port) {
  *	@param[in] address - address of target
  *	@param[in] ports - list of ports to be scanned.
  */
+
 void Target::TCP_Scan_Ports(const string& address, const vector<int>& ports) {
 	vector <thread *> scanning_tasks;
 
@@ -162,7 +364,18 @@ void Target::TCP_Scan_Ports(const string& address, const vector<int>& ports) {
  *	@name TCP_Print_Open_Ports
  *	@brief Sorts the open_ports vector and prints to user.
  */
+
 void Target::TCP_Print_Open_Ports() {
 	sort(open_ports.begin(), open_ports.end());
 	for (int i = 0; i < open_ports.size(); i++) cout << open_ports[i] << endl;
+}
+
+/*
+ *	@name Get_Address 
+ *	@brief Accessor function for targets address.
+ *  @return Const reference to targets address.
+ */
+
+const string& Target::Get_Address() {
+	return address;
 } 
