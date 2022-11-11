d63dc9145fb3c0028b9499fc5339bfff34d6e6c3
ankitm28 <amistry2@vols.utk.edu>
2022-11-10 19:54:41 -0500

    added scanning targets

--- spvs_cli/src/port_scanner.cpp
+++ spvs_cli/src/port_scanner.cpp
@@ -2,91 +2,135 @@
  * Ankit Mistry
  * Last Updated October 27, 2022 08:08 PM
  * Originally created by Mason Hyman, modified by me.
- * 
+ *
  * Scans for open ports on a given IP address or web address. Works on both stdin and file read.
-*/
+ */
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
+#include <cstdlib>
 #include <list>
-#include <vector>
+#include <algorithm>
 #include <thread>
+#include <mutex>
+#include <chrono>
+#include <ctime>
+#include <locale>
 #include <SFML/Network.hpp>
 
+#include "spvs.h"
+
 using namespace std;
+mutex m;
 
-void print_usage() {
-	cerr <<  "Usage: spvs [Options] {target specification}" << endl
-		  << "TARGET SPECIFICATION:" << endl
-		  << "    -f <input file name>: Input from list of hosts." << endl;
+static bool port_is_open(const string &address, int port)
+{
+    return (sf::TcpSocket().connect(sf::IpAddress::resolve(address).value(), port, sf::milliseconds(100)) == sf::Socket::Status::Done);
 }
 
-static bool port_is_open(const string& address, int port) {
-	return (sf::TcpSocket().connect(sf::IpAddress::resolve(address).value(), port, sf::milliseconds(100)) == sf::Socket::Status::Done);
+void threaded_port_is_open(const string &address, int port, vector<bool> &status)
+{
+    cout << address << endl;
+    if (sf::TcpSocket().connect(sf::IpAddress::resolve(address).value(), port) == sf::Socket::Status::Done)
+    {
+        status.at(port) = 1;
+    }
 }
 
-void threaded_port_is_open(const string& address, int port, vector<bool>& status) {
-	cout << address << endl;	
-	if(sf::TcpSocket().connect(sf::IpAddress::resolve(address).value(), port) == sf::Socket::Status::Done) {
-		status.at(port) = 1;
-	}
+void port_scan(const string &address, int ports)
+{
+    vector<bool> open_ports(ports + 1, false);
+    vector<thread *> threads;
+
+    bool host_online = false;
+
+    for (int i = 1; i <= ports; i++)
+    {
+        if (port_is_open(address, i))
+        {
+            host_online = true;
+            open_ports.at(i) = true;
+            cout << "PORT " << i << " IS OPEN\n";
+        }
+    }
+
+    if (host_online)
+    {
+        cout << address << " is ONLINE with open ports: " << endl;
+        for (int i = 1; i < open_ports.size(); i++)
+        {
+            if (open_ports.at(i))
+                cout << "   Port " << i << " is open" << endl;
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << address << " is OFFLINE." << endl;
+    }
 }
 
-void port_scan(const string& address, int ports) {
-	vector <bool> open_ports(ports+1, false);
-	vector <thread *> threads;
-
-	bool host_online = false;
-	
-	for (int i = 1; i <= ports; i++) {
-		if (port_is_open(address, i)) {
-			host_online = true;
-			open_ports.at(i) = true;
-			cout << "PORT " << i << " IS OPEN\n";
-		} 	
-	}
-
-	if (host_online) {
-		cout << address << " is ONLINE with open ports: " << endl;
-		for (int i = 1; i < open_ports.size(); i++) {
-			if (open_ports.at(i)) cout << "   Port " << i << " is open" << endl;
-		}
-     	cout << endl;
-	} else {
-		cout << address << " is OFFLINE." << endl;
-	}
+int main(int argc, char **argv)
+{
+    SPVS(argc, argv);
+    return 0;
 }
 
-int main(int argc, char ** argv) {
-		
-	list<string> hosts;
-	list<string>::iterator lit;
-   string host;
-   ifstream file;
+/* Code taken from main.cpp */
+SPVS::SPVS(int &argc, char **argv) 
+{
+    vector <Target *> trgts;
 
-	/* PARSE USER COMMANDS */
-	if (argc == 1) { print_usage(); return 1; }	
-	else if (argc == 2) {
-		/* If only two arguments, second arg must be a hostname */
-		hosts.push_back(argv[1]);
-	}
-	else if (argc == 3) {
-		if (string(argv[1]) == "-f") {
-			/* READ HOSTS FROM FILE */
-         file.open(argv[2]);
-			if(!file.is_open()) cerr << "Could not open file " << argv[1] << endl;
-				
-			while (file >> host) hosts.push_back(host); 
-			file.close();
-			cout << "Successfully read in " << hosts.size() << " hosts" << endl;
-		}
-	} 
+    if (argc == 1) {
+        cerr << "Usage: spvs [options] {target specification}" << endl
+             << "TARGET SPECIFICATION:" << endl
+             << "    -f <input file name>: Input from list of hosts." << endl
+             << "    -p <port range>: Single port or port range specified start-end." << endl
+             << "        Ex: spvs -p 10     Scans only port 10." << endl
+             << "        Ex: spvs -p 10-100 Scans only 10 through 100." << endl;
+    }
 
-	/* PERFORM BASIC DISCOVERY */
-	/* Essentially determining if host is online */
-	for (lit = hosts.begin(); lit != hosts.end(); lit++) {
-		port_scan(*lit, 65535);
-   }
+    auto start_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << "Starting SPVS at " << ctime(&start_time);
 
-	return 0;
-} 
+    this->input = new Input(argc, argv);
+    
+    if (this->input->Arg_Exists("-f")) {
+        this->targets = this->input->Get_Targets_From_File(this->input->Get_Next_Arg("-f"));
+    }
+
+    if (this->input->Arg_Exists("-p")) {
+        this->ports_to_scan = this->input->Get_Ports_To_Scan(this->input->Get_Next_Arg("-p"));
+        this->services = new Services("references/services");
+    } else {
+        this->services = new Services("references/top_ports");
+    }
+
+    trgts = this->input->Get_Targets_From_Stdin();
+    targets.insert(targets.end(), trgts.begin(), trgts.end());
+
+    Scan_Targets();
+    Print_Targets();
+
+    auto end_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << "Successfully completed: " << targets.size() << " hosts scanned " << difftime(end_time, start_time) << " seconds." << endl;
+}
+
+void SPVS::Scan_Targets() 
+{
+    vector <Target *>::const_iterator targetit;
+    Target *tmpt;
+
+    for (targetit = targets.begin(); targetit != targets.end(); targetit++) {
+        tmpt = *targetit;
+
+        if (!tmpt->ICMP_Host_Online(tmpt->Get_Address())) {
+            tmpt->Set_Online(false);
+            break;
+        }
+
+        tmpt->Set_Online(true);
+        tmpt->TCP_Scan_Ports(tmpt->Get_Address(), ports_to_scan);
+    }
+}
